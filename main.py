4b1c749e294f5d952d59785c3181a9483d7b813a
ex-test228 <magt9014@gmail.com>
2025-05-16 21:29:14 +0900

    スタイルの適用のためstyle.cssを追加

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-# from flask import Flask, render_template
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def hello_world():
-#     return render_template('index.html')
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
 import os
 from flask import Flask, redirect, render_template, request, jsonify, session, url_for
 import sqlite3
